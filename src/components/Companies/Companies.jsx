ddf458885efdbcc595134e60a220920392305709
Cattie <87996502+Catherine-Awuletey@users.noreply.github.com>
2024-10-31 20:16:18 +0000

    Complete Contacts

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Companies.css";
+
+const Companies = () => {
+  return (
+    <sectio className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="Prologis" />
+        <img src="./tower.png" alt="Tower" />
+        <img src="./equinix.png" alt="Equinix" />
+        <img src="./realty.png" alt="Realty" />
+      </div>
+    </sectio>
+  );
+};
+
+export default Companies;
